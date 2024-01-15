ad31e6f7dd53f3a98fd672931abbb6605c46732e
ktruedat <crinnic77@gmail.com>
2024-01-15 21:19:27 +0200

    setting max processors for the runtime

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"os"
+	"runtime"
 )
 
 var build = "develop"
@@ -28,6 +30,22 @@ func main() {
 
 // Entry level run function for entering the program
 func run(log *zap.SugaredLogger) error {
+
+	// ================================================================
+	// GOMAXPROCS
+
+	// Set the correct number of threads for the service
+	// based on what is available either by the machine or quotas
+	if _, err := maxprocs.Set(); err != nil {
+		return fmt.Errorf("maxprocs: %w", err)
+	}
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// ===============================================================
+	// Configuration
+
+	cfg := struct {
+	}{}
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.21
 
 require go.uber.org/zap v1.26.0
 
-require go.uber.org/multierr v1.11.0 // indirect
+require (
+	go.uber.org/automaxprocs v1.5.3 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+)
