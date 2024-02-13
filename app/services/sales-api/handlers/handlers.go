9e4151e5ad61a050afd3ae5555eae0c238158b44
ktruedat <crinnic77@gmail.com>
2024-02-13 23:09:33 +0200

    function notes

--- app/services/sales-api/handlers/handlers.go
+++ app/services/sales-api/handlers/handlers.go
@@ -6,9 +6,14 @@ import (
 	"net/http/pprof"
 )
 
+// DebugStandardLibraryMux registers all the debug routes from the standard library
+// into a new mux bypassing in the use of DefaultServerMux. Using the
+// DefaultServerMux would be a security risk since a dependency could inject a
+// handler into our service without us knowing it
 func DebugStandardLibraryMux() *http.ServeMux {
 	mux := http.NewServeMux()
 
+	// Register all the standard library endpoints
 	mux.HandleFunc("/debug/pprof/", pprof.Index)
 	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
 	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
