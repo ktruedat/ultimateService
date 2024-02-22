419844b9dcffbf37ed4b6c4969705d7a2be25822
ktruedat <crinnic77@gmail.com>
2024-02-22 23:07:30 +0200

    validate package under sys layer: business related errors and validations for the app

--- business/sys/validate/errors.go
+++ business/sys/validate/errors.go
@@ -2,8 +2,10 @@ package validate
 
 import "errors"
 
+// ErrInvalidID occurs when an ID is not in a valid form.
 var ErrInvalidID = errors.New("not a proper ID value")
 
+// ErrorResponse is the form used for API responses from failures in the API.
 type ErrorResponse struct {
 	Error  string `json:"error"`
 	Fields string `json:"fields,omitempty"`
