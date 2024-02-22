29d830d0b6cd5943579ee58a9cbfc6331ff82d0d
ktruedat <crinnic77@gmail.com>
2024-02-22 23:16:04 +0200

    request error struct

--- business/sys/validate/errors.go
+++ business/sys/validate/errors.go
@@ -10,3 +10,23 @@ type ErrorResponse struct {
 	Error  string `json:"error"`
 	Fields string `json:"fields,omitempty"`
 }
+
+// RequestError is used to pass an error during the request through the
+// application with web specific context
+type RequestError struct {
+	Err    error
+	Status int
+	Fields error
+}
+
+// NewRequestError wraps a provided error with an HTTP status code. This
+// function should be used when handlers encounter expected errors.
+func NewRequestError(err error, status int) error {
+	return &RequestError{err, status, nil}
+}
+
+// Error implements the error interface. It uses the default message of the
+// wrapped error. This is what will be shown in the services' logs.
+func (err *RequestError) Error() string {
+	return err.Err.Error()
+}
