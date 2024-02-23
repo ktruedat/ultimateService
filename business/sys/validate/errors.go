5d7cb182334c5b10da9a433947781cd5fccf3783
ktruedat <crinnic77@gmail.com>
2024-02-23 22:37:39 +0200

    Cause function fer returning the first caller of the error

--- business/sys/validate/errors.go
+++ business/sys/validate/errors.go
@@ -39,8 +39,10 @@ type FieldError struct {
 	Error string `json:"error"`
 }
 
+// FieldErrors represents a collection of field errors.
 type FieldErrors []FieldError
 
+// Error implements the error interface.
 func (fe FieldErrors) Error() string {
 	d, err := json.Marshal(fe)
 	if err != nil {
@@ -49,6 +51,8 @@ func (fe FieldErrors) Error() string {
 	return string(d)
 }
 
+// Cause iterates through all the wrapped errors until the root
+// error value is reached.
 func Cause(err error) error {
 	root := err
 	for {
